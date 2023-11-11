3fb8e14ed90496373b4fdb6281f2e1f4dd3d4441
KoEunsaem <koensm12@gmail.com>
2023-11-11 12:08:03 +0900

    mysql connection

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const mysql = require("mysql2");
 
 const app = express();
+
+const connection = mysql.createConnection({
+    host: 'dofaming-mysql.co4ylmymttoe.ap-northeast-2.rds.amazonaws.com',
+    user: 'root',
+    password: 'Tkfrnrtn',
+    database: 'dofamingdb'
+});
+
+connection.connect((err) => {
+    if (err) {
+      console.error('MySQL 연결 오류:', err);
+      return;
+    }
+    console.log('MySQL database 연결 성공!!');
+  });
+
+  connection.end((err) => {
+    if (err) {
+      console.error('MySQL 연결 해제 오류:', err);
+      return;
+    }
+    console.log('MySQL database 연결 해제 성공!!');
+  });
