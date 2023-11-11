f998a5721a84c1f149a149caf6c387bc16615b7a
mini-777 <pho0570@gmail.com>
2023-11-11 10:44:18 +0900

    Add Tamagui dependencies and configuration.

--- frontend/Dofarming/App.js
+++ frontend/Dofarming/App.js
@@ -1,5 +1,36 @@
-import { Button } from 'tamagui'
+import { Button, Stack } from 'tamagui'
+import { TamaguiProvider } from 'tamagui'
+import config from './tamagui.config'
+import { useFonts } from 'expo-font'
+import React, { useEffect } from 'react'
+import { SafeAreaView } from 'react-native'
 
 export default function App() {
-  return <Button>Hello world</Button>
+  const [loaded] = useFonts({
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      // can hide splash screen here
+    }
+  }, [loaded])
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <TamaguiProvider config={config}>
+      <SafeAreaView flex={1}>
+        <Stack>
+          <Button>Hello world</Button>
+
+        </Stack>
+
+      </SafeAreaView>
+    </TamaguiProvider>)
 }
+
+
